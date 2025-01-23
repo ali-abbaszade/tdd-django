59d10fd953690a9be47023172ef95bc11b7eb93e
Ali Abbaszade <abbasszade.a@gmail.com>
2025-01-23 18:51:48 +0330

    Updated admin
    Registered models

--- app/books/admin.py
+++ app/books/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from books import models
+
+
+class BookAuthorInline(admin.TabularInline):
+    model = models.BookAuthor
+
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "publication_date"]
+    list_per_page = 10
+    list_filter = ["publication_date"]
+    search_fields = ["title"]
+    inlines = [BookAuthorInline]
+
+
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
